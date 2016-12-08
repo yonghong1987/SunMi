805ed212a26f8c151d6b831df869da079259babf
yonghong1987 <cyh@KavendeMacBook-Pro.local>
2016-12-08 16:03:59 +0800

    55

---
+++ SunMicromobile/SMMakeViewController.h
@@ -0,0 +1,13 @@
+//
+//  SMMakeViewController.h
+//  SunMicromobile
+//
+//  Created by cyh on 16/12/8.
+//  Copyright © 2016年 Evan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SMMakeViewController : UIViewController
+
+@end
---
+++ SunMicromobile/SMMakeViewController.m
@@ -0,0 +1,37 @@
+//
+//  SMMakeViewController.m
+//  SunMicromobile
+//
+//  Created by cyh on 16/12/8.
+//  Copyright © 2016年 Evan. All rights reserved.
+//
+
+#import "SMMakeViewController.h"
+
+@interface SMMakeViewController ()
+
+@end
+
+@implementation SMMakeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
